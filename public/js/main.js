95bcd2302c8303361127e1dfa04f0bafd86dec92
Daniel Rodriguez <99danielrod@gmail.com>
2020-06-10 15:36:44 -0500

    front develpment

---
+++ public/js/main.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+    
+        $('.bxslider').bxSlider({
+          mode: 'fade',
+          captions: true,
+          slideWidth: 1200
+        });
+        checkTheme();
+    var posts=[
+        {
+            title: 'Prueba de titulo1',
+            date: 'Publicado: '+ moment().format("Do MMMM YYYY"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dignissim gravida quam, ut lacinia metus pulvinar posuere. Donec sit amet risus tincidunt augue semper mattis. Sed sagittis, odio et auctor imperdiet, quam orci mattis enim, fringilla sollicitudin velit metus eu massa. Donec tortor orci, blandit sit amet lectus sed, consequat malesuada leo. Proin nec congue dolor. Phasellus suscipit viverra tellus a egestas. Phasellus dapibus et magna vel eleifend. Fusce lectus metus, cursus at dui sed, posuere bibendum arcu. Suspendisse semper porta nisl ut laoreet. Maecenas eu tortor ac lacus molestie pulvinar. '
+        },{
+            title: 'Prueba de titulo2',
+            date: 'Publicado: '+ moment().format("Do MMMM YYYY"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dignissim gravida quam, ut lacinia metus pulvinar posuere. Donec sit amet risus tincidunt augue semper mattis. Sed sagittis, odio et auctor imperdiet, quam orci mattis enim, fringilla sollicitudin velit metus eu massa. Donec tortor orci, blandit sit amet lectus sed, consequat malesuada leo. Proin nec congue dolor. Phasellus suscipit viverra tellus a egestas. Phasellus dapibus et magna vel eleifend. Fusce lectus metus, cursus at dui sed, posuere bibendum arcu. Suspendisse semper porta nisl ut laoreet. Maecenas eu tortor ac lacus molestie pulvinar. '
+        },{
+
+            title: 'Prueba de titulo3',
+            date: 'Publicado: '+ moment().format("Do MMMM YYYY"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dignissim gravida quam, ut lacinia metus pulvinar posuere. Donec sit amet risus tincidunt augue semper mattis. Sed sagittis, odio et auctor imperdiet, quam orci mattis enim, fringilla sollicitudin velit metus eu massa. Donec tortor orci, blandit sit amet lectus sed, consequat malesuada leo. Proin nec congue dolor. Phasellus suscipit viverra tellus a egestas. Phasellus dapibus et magna vel eleifend. Fusce lectus metus, cursus at dui sed, posuere bibendum arcu. Suspendisse semper porta nisl ut laoreet. Maecenas eu tortor ac lacus molestie pulvinar. '
+        },{
+            title: 'Prueba de titulo4',
+            date: 'Publicado: '+ moment().format("Do MMMM YYYY"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dignissim gravida quam, ut lacinia metus pulvinar posuere. Donec sit amet risus tincidunt augue semper mattis. Sed sagittis, odio et auctor imperdiet, quam orci mattis enim, fringilla sollicitudin velit metus eu massa. Donec tortor orci, blandit sit amet lectus sed, consequat malesuada leo. Proin nec congue dolor. Phasellus suscipit viverra tellus a egestas. Phasellus dapibus et magna vel eleifend. Fusce lectus metus, cursus at dui sed, posuere bibendum arcu. Suspendisse semper porta nisl ut laoreet. Maecenas eu tortor ac lacus molestie pulvinar. '
+        }
+    ];
+    posts.forEach((item,index)=>{
+        var post=`
+        <article class="post">
+         <h2>${item.title}</h2>
+        <p class="date">${item.date}</p>
+        <div class="clearfix">${item.content}</div>
+        <p class="contenido">${item.content}</p>
+        <a href="#" class="boton">Leer Mas</a>
+        </article>
+        `
+        $("#posts").append(post);
+    })
+    var tema=$("#theme");
+    $("#toGreen").click(function(){
+        tema.attr("href","/public/css/green.css");
+        localStorage.setItem('color','green');
+    })
+    $("#toRed").click(function(){
+        tema.attr("href","/public/css/red.css");
+        localStorage.setItem('color','red');
+    })
+    $("#toBlue").click(function(){
+        tema.attr("href","/public/css/blue.css");
+        localStorage.setItem('color','blue');
+    })
+    $(".subir").click((e)=>{
+        e.preventDefault();
+        $("html,body").animate({
+            scrollTop:0
+        },500)
+        return false;
+    })
+})
+
+function checkTheme(){
+    var color=localStorage.getItem('color');
+    var tema=$("#theme");
+    switch(color){
+        case 'green':
+            tema.attr("href","/public/css/green.css");
+        break;
+        case 'blue':
+            tema.attr("href","/public/css/blue.css");
+        break;
+        case 'red':
+            tema.attr("href","/public/css/red.css");
+        break;
+        default:
+            tema.attr("href","/public/css/green.css");
+        break;
+    }
+}
